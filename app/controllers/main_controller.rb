070716cb516bb9b16649c746ca1916b88eb7f024
adna <adna@adna.com>
2018-07-18 01:39:28 +0800

    Add basic CRUD

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,8 +2,4 @@ class MainController < ApplicationController
   def index
     @pokemons = Pokemon.all
   end
-
-  def show_details
-    @pokemon = Pokemon.find( Integer(params[:id]) )
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   root 'main#index'
-  get ':id' => 'main#show_details'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'pokemon/:id/read' => 'pokemon#show_details'
+  get 'pokemon/create' => 'pokemon#create_form'
+  post 'pokemon/create' => 'pokemon#create'
+  get 'pokemon/:id/update' => 'pokemon#update_form'
+  post 'pokemon/:id/update' => 'pokemon#update'
+  post 'pokemon/:id/delete' => 'pokemon#delete'
+
 end
