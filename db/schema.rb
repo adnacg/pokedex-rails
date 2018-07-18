39feafce0c6dd240113092299595aeec1c09d1f2
adna <adna@adna.com>
2018-07-19 00:11:53 +0800

    Add data migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_18_131949) do
+ActiveRecord::Schema.define(version: 2018_07_18_160624) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2018_07_18_131949) do
     t.bigint "pokemon_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "gender"
     t.index ["pokemon_id"], name: "index_masters_on_pokemon_id"
   end
 
