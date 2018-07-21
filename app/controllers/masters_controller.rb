8772e256ca0c5cbf6f3c2bbb9c4f96186eb9235f
adna <adna@adna.com>
2018-07-21 13:00:10 +0800

    WIP

--- app/controllers/masters_controller.rb
+++ app/controllers/masters_controller.rb
@@ -1,6 +1,14 @@
 class MastersController < ApplicationController
   def index
-    @masters = Master.all
+    # @masters = Master.all
+      # test to see if we are at /pokemons/:pokemon_id/masters or /masters
+      if params.has_key?(:pokemon_id)
+        # get all the masters for a specific pokemon
+        @masters = Master.where(pokemon_id: params[:pokemon_id] )
+      else
+        # get all masters
+        @masters = Master.all
+      end
   end
 
   def create
